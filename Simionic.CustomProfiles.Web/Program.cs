7da4c768b817e685be432b7120039e86dc215264
Neil Hewitt <neil.hewitt@gmail.com>
2022-02-05 19:51:25 +0000

    Identity + graph integration complete and refactored. Move Blazor project to .NET 6

--- Simionic.CustomProfiles.Web/Program.cs
+++ Simionic.CustomProfiles.Web/Program.cs
@@ -1,17 +1,12 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
-using Simionic.CustomProfiles.Web.Graph;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Simionic.CustomProfiles.Web.Graph;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Simionic.CustomProfiles.Web
 {
@@ -20,37 +15,21 @@ namespace Simionic.CustomProfiles.Web
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
             builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            // <HttpClientSnippet>
-            // HttpClient for passing into GraphServiceClient constructor
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://graph.microsoft.com") });
-            // </HttpClientSnippet>
-
-            // <AddMsalAuthSnippet>
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://graph.microsoft.com") }); // use MS Graph to get extended user info
+            
             builder.Services.AddMsalAuthentication<RemoteAuthenticationState, RemoteUserAccount>(options =>
             {
-                var scopes = builder.Configuration.GetValue<string>("GraphScopes");
-                if (string.IsNullOrEmpty(scopes))
-                {
-                    Console.WriteLine("WARNING: No permission scopes were found in the GraphScopes app setting. Using default User.Read.");
-                    scopes = "User.Read";
-                }
-
-                foreach(var scope in scopes.Split(';'))
-                {
-                    Console.WriteLine($"Adding {scope} to requested permissions");
-                    options.ProviderOptions.DefaultAccessTokenScopes.Add(scope);
-                }
-
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("User.Read"); // need this scope to get user info from Graph
                 builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+                options.ProviderOptions.LoginMode = "redirect";
             })
-            .AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, RemoteUserAccount, GraphUserAccountFactory>();
-            // </AddMsalAuthSnippet>
-
-            // <AddGraphClientFactorySnippet>
-            builder.Services.AddScoped<GraphClientFactory>();
-            // </AddGraphClientFactorySnippet>
+            .AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, RemoteUserAccount, UserAccountFactory>();
+            
+            builder.Services.AddScoped<GraphServiceClientFactory>();
 
             await builder.Build().RunAsync();
         }
