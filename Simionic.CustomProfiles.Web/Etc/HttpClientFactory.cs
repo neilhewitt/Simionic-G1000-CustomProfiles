21691bd0ad65d4f3c510855adf2eddffd78a3871
Neil Hewitt <neil.hewitt@gmail.com>
2022-02-23 14:29:38 +0000

    Wiring it all up...

--- Simionic.CustomProfiles.Web/Etc/HttpClientFactory.cs
+++ Simionic.CustomProfiles.Web/Etc/HttpClientFactory.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Simionic.CustomProfiles.Web
 {
     public static class HttpClientFactory
     {
-        public const string BaseAddress = "https://localhost:7071";
+        public const string BaseAddress = "https://simionic-g1000-profile-database-functions.azurewebsites.net/";
+        public const string ApiHostKey = "RdyGRIvTPLAkTlJGe8f8hOaFtdmZSK3sdcFCeKHf0239EqqwzsUc3w==";
 
-        public static HttpClient Client => new HttpClient() { BaseAddress = new Uri(BaseAddress) };
+        public static HttpClient Client
+        {
+            get
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BaseAddress);
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("x-functions-key", ApiHostKey);
+                return client;
+            }
+        }
+        
     }
 }
